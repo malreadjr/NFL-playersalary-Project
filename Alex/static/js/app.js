e371a680894855d3d37d55d1c77704b9fa209a46
alexturkmen <ulas.turkmen26@gmail.com>
2021-01-26 12:12:04 -0500

    second chart added.

--- Alex/static/js/app.js
+++ Alex/static/js/app.js
@@ -1,9 +1,9 @@
-function buildPlot() {
+function buildScatterPlot() {
   /* data route */
   const url = "/api/salaries";
   d3.json(url).then(function(data) {
 
-    console.log(data);
+    // console.log(data);
 
 function calcAvg(year_no) {
   let results = data.filter((item) => {
@@ -26,7 +26,7 @@ function calcAvg(year_no) {
 
 console.log(calcAvg(2015))
 
-var trace = {
+let trace = {
   type: "scatter",
   mode: "lines",
   // name: name,
@@ -37,31 +37,152 @@ var trace = {
   }
 };
 
+let dataScatter = [trace];
 
-
-var data = [trace];
-
-var layout = {
-  title: `Salary Data`,
-  // xaxis: {
+let layout = {
+  title: `Average Salary Data by Year`,
+  xaxis: {
+      title: "Years"
   //   range: [startDate, endDate],
   //   type: "date"
-  // },
-  // yaxis: {
-  //   autorange: true,
-  //   type: "linear"
-  // },
+  },
+  yaxis: {
+      title: "Salary in million $",
+    autorange: true,
+    type: "linear"
+  },
   showlegend: false
 };
 
-Plotly.newPlot("plot", data, layout);
-
+Plotly.newPlot("plot", dataScatter, layout);
 
   })
 
-//   .catch(function(error){
-//     // handle error   
-//  })
 }
 
-buildPlot();
+buildScatterPlot();
+
+
+
+
+
+
+
+
+function buildDropdownPlot(first_year) {
+  /* data route */
+  const url = "/api/salaries";
+  d3.json(url).then(function(response) {
+
+    console.log(response);
+
+
+  let resultsByYear = response.filter((item) => {
+  return item.year === parseInt(first_year);
+  })
+
+  // console.log(resultsByYear)
+
+  let yearsForGraph = resultsByYear.map((item) => {
+  return item.year;
+  })
+
+  console.log(yearsForGraph)
+
+  let salariesForGraph = resultsByYear.map((item) => {
+  return item.salary;
+  })
+
+  let namesForGraph = resultsByYear.map((item) => {
+    return item.player;
+    })
+
+    console.log(namesForGraph)
+
+  let trace1 = {
+  x: namesForGraph,
+  y: salariesForGraph,
+  mode: 'markers',
+  type: 'scatter',
+  marker: { size: 5 }
+  }
+
+  let data1 = [trace1]
+
+
+  let layout1 = {
+  xaxis: {
+    title: "Year"
+    // rangeslider: {}
+  },
+  yaxis: {
+    range: [0, 25000000]
+  },
+  title:'Salary Dropdown'
+  };
+
+  Plotly.newPlot('plot2', data1, layout1);
+
+})
+}
+
+
+
+
+
+
+function buildDropDown() {
+
+  // Creating a dropdown variable
+
+  let dropDown = d3.select("#selDataset");
+
+  // Using a forEach loop, append the options into #selDataset
+
+    let years = [2015, 2016, 2017, 2018, 2019, 2020]
+    years.forEach((item) => {
+      dropDown.append("option").text(item).property("value", item);
+    });
+
+  
+
+      // Handling the change event
+      
+      dropDown.on("change", handleSelect);
+
+      // Creating the handleSelect function     
+      
+      function handleSelect() {
+      let year_on_dropdown = dropDown.property("value");
+      console.log(year_on_dropdown)
+      // console.log(sampleID)
+      buildDropdownPlot(year_on_dropdown);
+      }
+
+      // Adding the dispatch function to add the plots to the screen when the page is loaded
+
+      dropDown.dispatch("change");
+
+
+  }
+
+  buildDropDown()
+
+  function clrchange()
+  {
+  // var bgcolor = document.getElementById('s1').value;
+  document.getElementById('selDataset').style.backgroundColor = "#158336d7";
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
